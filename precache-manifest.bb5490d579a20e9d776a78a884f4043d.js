cf3b95cec142c9d64847d747fbc5763095e3e45c
lim chee keat <cheekeat_lim1991@hotmail.com>
2021-08-27 11:17:07 +0800

    Updates

---
+++ precache-manifest.bb5490d579a20e9d776a78a884f4043d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61b30870d11df52c239f",
+    "url": "/PRTokenDistribution/static/css/main.8be34349.chunk.css"
+  },
+  {
+    "revision": "61b30870d11df52c239f",
+    "url": "/PRTokenDistribution/static/js/main.61b30870.chunk.js"
+  },
+  {
+    "revision": "85ed228f18b634b73445",
+    "url": "/PRTokenDistribution/static/css/1.24c0da15.chunk.css"
+  },
+  {
+    "revision": "85ed228f18b634b73445",
+    "url": "/PRTokenDistribution/static/js/1.85ed228f.chunk.js"
+  },
+  {
+    "revision": "7ba41ddf7e3afc948cf7",
+    "url": "/PRTokenDistribution/static/js/runtime~main.7ba41ddf.js"
+  },
+  {
+    "revision": "aacc700a69bf99ed47254fcf46507831",
+    "url": "/PRTokenDistribution/index.html"
+  }
+];
