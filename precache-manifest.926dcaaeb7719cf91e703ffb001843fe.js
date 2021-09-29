7a1ef3c2c66d0dab9feb71dfdfac2363c55713af
lim chee keat <cheekeat_lim1991@hotmail.com>
2021-09-29 09:23:45 +0800

    Updates

---
+++ precache-manifest.926dcaaeb7719cf91e703ffb001843fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84d2b3904f00b3c2723c",
+    "url": "/PRTokenBDL/static/css/main.8d497950.chunk.css"
+  },
+  {
+    "revision": "84d2b3904f00b3c2723c",
+    "url": "/PRTokenBDL/static/js/main.84d2b390.chunk.js"
+  },
+  {
+    "revision": "be016088ee49f9079273",
+    "url": "/PRTokenBDL/static/css/1.1a9f5a68.chunk.css"
+  },
+  {
+    "revision": "be016088ee49f9079273",
+    "url": "/PRTokenBDL/static/js/1.be016088.chunk.js"
+  },
+  {
+    "revision": "0e58fc9434be4045185d",
+    "url": "/PRTokenBDL/static/js/runtime~main.0e58fc94.js"
+  },
+  {
+    "revision": "249e3788b2c8a9eb9230f838cc9307c0",
+    "url": "/PRTokenBDL/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/PRTokenBDL/precache-manifest.22c84b3952d2636074637108402afb50.js"
+  "/PRTokenBDL/precache-manifest.926dcaaeb7719cf91e703ffb001843fe.js"
 );
 
 workbox.clientsClaim();
